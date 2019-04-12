a6d4169b1a6dfde02f62b3fb40d975762deab848
Muhammed Kostekli <muhammedkostekli@gmail.com>
2019-04-12 16:27:26 +0300

    Auto-Completion v1

--- Idefix Keyboard/KeyboardViewController.swift
+++ Idefix Keyboard/KeyboardViewController.swift
@@ -12,6 +12,10 @@ class KeyboardViewController: UIInputViewController {
 
     @IBOutlet var nextKeyboardButton: UIButton!
     
+    // Auto Complete Buttons
+    @IBOutlet var suggestionButton: [UIButton] = []
+    
+    
     var capsLockOn = true
     var currentCharSet = 0
     
@@ -22,6 +26,9 @@ class KeyboardViewController: UIInputViewController {
     
     @IBOutlet weak var charSet1: UIView!
     @IBOutlet weak var charSet2: UIView!
+    
+    // Saved Strings for Auto Complete
+    var savedStrings = ["Bafetimbi","Galatasaray","Galata","Dürüm"]
     
     override func updateViewConstraints() {
         super.updateViewConstraints()
@@ -35,7 +42,9 @@ class KeyboardViewController: UIInputViewController {
         let objects = nib.instantiate(withOwner: self, options: nil)
         view = objects[0] as? UIView
         
-        charSet2.isHidden = true
+        
+        charSet1.isHidden = true
+        clearSuggestionButtons()
     }
     
     override func textWillChange(_ textInput: UITextInput?) {
@@ -44,7 +53,6 @@ class KeyboardViewController: UIInputViewController {
     
     override func textDidChange(_ textInput: UITextInput?) {
         // The app has just changed the document's contents, the document context has been updated.
-        
         var textColor: UIColor
         let proxy = self.textDocumentProxy
         if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
@@ -53,7 +61,6 @@ class KeyboardViewController: UIInputViewController {
             textColor = UIColor.black
         }
     }
-    
     
     
     // Keyboard Buttons Controller
@@ -73,9 +80,12 @@ class KeyboardViewController: UIInputViewController {
     @IBAction func keyPressed(button: UIButton) {
         let string = button.titleLabel!.text
         (textDocumentProxy as UIKeyInput).insertText("\(string!)")
-        
+        let currentStr = (textDocumentProxy.documentContextBeforeInput ?? "") + (textDocumentProxy.documentContextAfterInput ?? "")
+        DispatchQueue.main.async {
+            self.changeSuggestionButtons(inputText: currentStr)
+        }
         button.transform = CGAffineTransform.identity
-        UIView.animate(withDuration: 0.2, animations: {
+        UIView.animate(withDuration: 0.05, animations: {
             button.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
         }, completion: {(_) -> Void in
             button.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
@@ -84,6 +94,10 @@ class KeyboardViewController: UIInputViewController {
     
     @IBAction func backSpacePressed(button: UIButton) {
         (textDocumentProxy as UIKeyInput).deleteBackward()
+        let currentStr = (textDocumentProxy.documentContextBeforeInput ?? "") + (textDocumentProxy.documentContextAfterInput ?? "")
+        DispatchQueue.main.async {
+            self.changeSuggestionButtons(inputText: currentStr)
+        }
     }
     
     @IBAction func spacePressed(button: UIButton) {
@@ -96,12 +110,12 @@ class KeyboardViewController: UIInputViewController {
     
     @IBAction func charSetPressed(button: UIButton) {
         if currentCharSet == 0 {
-            charSet1.isHidden = true
-            charSet2.isHidden = false
-            currentCharSet = 1
-        } else {
             charSet1.isHidden = false
             charSet2.isHidden = true
+            currentCharSet = 1
+        } else {
+            charSet1.isHidden = true
+            charSet2.isHidden = false
             currentCharSet = 0
         }
     }
@@ -139,6 +153,41 @@ class KeyboardViewController: UIInputViewController {
                 
             }
         }
+    }
+    
+    // Check for Suggestion Buttons
+    func changeSuggestionButtons(inputText: String){
+        clearSuggestionButtons()
+        for str in savedStrings{
+            if(str.lowercased().contains(inputText.lowercased()) && str.count >= inputText.count){
+                for button in suggestionButton{
+                    if button.title(for: .normal) == "" && !wordIsAlreadySuggest(inputText: str){
+                        button.setTitle(str, for: .normal)
+                        break
+                    }
+                }
+            }
+        }
+        
+        
+    }
+    
+    // Clear title of suggestion buttons
+    func clearSuggestionButtons(){
+        for button in suggestionButton{
+            button.setTitle("", for: .normal)
+        }
+    }
+    
+    
+    // Control word is already suggested
+    func wordIsAlreadySuggest(inputText: String) -> Bool{
+        for button in suggestionButton{
+            if button.title(for: .normal) == inputText{
+                return true
+            }
+        }
+        return false
     }
 
 }
